c5607f258bb1f6106af84e9bb6cc1081baa57e23
Wataru Shimota <w.shimota@gmail.com>
2023-05-16 20:12:23 +0900

    added functional component

---
+++ src/HelloWorld.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import './HelloWorld.css';
+
+function HelloWorld(props) {
+  return (
+    <>
+      <hr/>
+      <h1 className='h1-green'>{props.msg}</h1>
+    </>
+  )
+}
+
+export default HelloWorld;
